c9d320e8050bdc5c9386c99d4a7fdd513a49a838
D47R48 <ayushyadav212121@gmail.com>
2023-07-10 01:34:07 +0530

    small changes

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -6,25 +6,26 @@ import About from '../Components/Landing/About'
 const LandingPage = () => {
   return (
     <>
-    <Herosection />
-    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 my-[4rem]"></hr>
-    <About />
-    <hr class="h-px my-12 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-    <section className="bg-gray-200 p-[60px] dark:bg-gray-900 mb-[-7rem]">
+      <Herosection />
+      <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 my-[4rem]"></hr>
+      <About />
+      <hr class="h-px my-12 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <section className="bg-gray-200 p-[60px] dark:bg-gray-900 mb-[-7rem]">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto mt-4 lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Payments tool for software companies
+              Ignite Your Team's Potential with Synergy in Motion
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              From checkout to global sales tax compliance, companies around the
-              world use Flowbite to simplify their payment stack.
+
+              Our platform enables seamless scheduling by allowing members to effortlessly share their availability, ensuring accurate meeting times with each individual's busy or free time slots.
+
             </p>
             <a
               href="#"
               className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-              style={{background:"#E83678"}}
-              
+              style={{ background: "#E83678" }}
+
             >
               Get started
               <svg
@@ -42,8 +43,8 @@ const LandingPage = () => {
             </a>
           </div>
           <div className="hidden md:mt-[-6rem] lg:mt-[-6rem] lg:col-span-5 lg:flex"
-          style={{marginTop : "-6rem"}}>
-           <lottie-player
+            style={{ marginTop: "-6rem" }}>
+            <lottie-player
               src="./business-team.json"
               background="transparent"
               speed="1"
